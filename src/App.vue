c8997cfd1d32d12ad318975b96998b4d14fe3c1f
megos <gmegos.27@gmail.com>
2019-03-27 17:47:32 +0900

    Fix component name

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,18 @@
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <Top />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Top from './components/Top'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Top
   },
   data () {
     return {
