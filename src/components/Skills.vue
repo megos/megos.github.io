d3de2513a975e15894abafe6bf1206a726fbc43b
megos <gmegos.27@gmail.com>
2019-03-28 16:04:43 +0900

    Add skills page

---
+++ src/components/Skills.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <cloud
+      :data="words"
+      :font-size-mapper="fontSizeMapper"
+    />
+  </v-container>
+</template>
+
+<script>
+import Cloud from 'vue-d3-cloud'
+
+export default {
+  components: {
+    Cloud,
+  },
+  data: () => ({
+    words: [
+      { text: 'Vue', value: 1000 },
+      { text: 'js', value: 200 },
+      { text: 'is', value: 800 },
+      { text: 'very cool', value: 1000000 },
+      { text: 'lunch', value: 100 },
+    ],
+    fontSizeMapper: word => Math.log2(word.value) * 5,
+  }),
+}
+</script>
