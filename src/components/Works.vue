2e7266bc84db2ab222ef2286bd63f8f95be12d15
megos <gmegos.27@gmail.com>
2019-04-02 16:36:55 +0900

    Fix prop name

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -6,26 +6,23 @@
     >
       <v-timeline>
         <v-timeline-item
-          v-for="(year, i) in years"
+          v-for="(d, i) in data"
           :key="i"
-          :color="year.color"
+          :color="d.color"
           small
         >
           <template v-slot:opposite>
             <span
-              :class="`headline font-weight-bold ${year.color}--text`"
-              v-text="year.year"
+              :class="`headline font-weight-bold ${d.color}--text`"
+              v-text="d.ym"
             />
           </template>
           <div class="py-3">
-            <h2 :class="`headline font-weight-light mb-3 ${year.color}--text`">
-              Lorem ipsum
+            <h2 :class="`headline font-weight-light mb-3 ${d.color}--text`">
+              {{ d.title }}
             </h2>
             <div>
-              Lorem ipsum dolor sit amet, no nam oblique veritus.
-              Commune scaevola imperdiet nec ut, sed euismod convenire principes at.
-              Est et nobis iisque percipit,
-              an vim zril disputando voluptatibus, vix an salutandi sententiae.
+              {{ d.text }}
             </div>
           </div>
         </v-timeline-item>
@@ -37,9 +34,11 @@
 <script>
 export default {
   data: () => ({
-    years: [{
-      year: '2018',
+    data: [{
+      ym: '2018/03',
       color: 'cyan',
+      title: 'hoge',
+      text: 'hogefuga',
     }],
   }),
 }
