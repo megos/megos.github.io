df8298284b78514443697197f274998fb1954377
megos <gmegos.27@gmail.com>
2019-04-02 14:54:43 +0900

    Add timeline example

---
+++ src/components/Works.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-timeline>
+        <v-timeline-item
+          v-for="(year, i) in years"
+          :key="i"
+          :color="year.color"
+          small
+        >
+          <template v-slot:opposite>
+            <span
+              :class="`headline font-weight-bold ${year.color}--text`"
+              v-text="year.year"
+            />
+          </template>
+          <div class="py-3">
+            <h2 :class="`headline font-weight-light mb-3 ${year.color}--text`">
+              Lorem ipsum
+            </h2>
+            <div>
+              Lorem ipsum dolor sit amet, no nam oblique veritus.
+              Commune scaevola imperdiet nec ut, sed euismod convenire principes at.
+              Est et nobis iisque percipit,
+              an vim zril disputando voluptatibus, vix an salutandi sententiae.
+            </div>
+          </div>
+        </v-timeline-item>
+      </v-timeline>
+    </v-layout>
+  </v-container>
+</template>
