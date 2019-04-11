cab8e7bddf18d80cc5dd5ab94d2a45dd86898679
megos <gmegos.27@gmail.com>
2019-04-11 12:03:45 +0900

    Add menu component

---
+++ src/components/ToolbarMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn
+    flat
+    @click="$router.push(content)"
+  >
+    <span class="mr-2">{{ content }}</span>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
