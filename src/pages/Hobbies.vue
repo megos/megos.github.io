ca869f380dd054ac96926631f7bddcbd62d0b501
megos <gmegos.27@gmail.com>
2019-04-11 12:23:43 +0900

    Add Hobby page

---
+++ src/pages/Hobbies.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        👷
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
+
+<script>
+export default {
+
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Top from '@/pages/Top'
 import Skills from '@/pages/Skills'
 import Works from '@/pages/Works'
+import Hobbies from '@/pages/Hobbies'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ const router = new Router({
       path: '/works',
       name: 'Works',
       component: Works,
+    },
+    {
+      path: '/hobbies',
+      name: 'Hobbies',
+      component: Hobbies,
     },
   ],
 })
